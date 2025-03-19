81b5850c8825971a20e222dbcc50a7c9269e6a1d
TANISHQ SHARMA <kundalashman88@gmail.com>
2025-03-19 10:58:50 +0530

    Solution to Input and Print a Double

---
+++ Input and Print a Double@@6788d5012db25efec946f942/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    double number; // Declare a double variable
+
+    printf("");
+    scanf("%lf", &number); // Read a double value
+
+    // Print the entered double
+    printf("You entered: %.2lf\n", number); // Print with 2 decimal places
+
+    return 0;
+}
